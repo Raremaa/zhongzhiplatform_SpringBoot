186fd7d7d9507290801196e128ca2444171c0d24
masaiqi <masaiqi1996@icloud.com>
2018-10-17 14:52:00 +0800

    init

---
+++ src/main/java/com/masaiqi/zhongzhi/ZhongzhiApplication.java
@@ -0,0 +1,12 @@
+package com.masaiqi.zhongzhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhongzhiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhongzhiApplication.class, args);
+    }
+}
