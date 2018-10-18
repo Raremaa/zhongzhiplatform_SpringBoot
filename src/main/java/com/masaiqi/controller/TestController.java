050118a2028ed91272ff1118090168a345d6abb9
masaiqi <masaiqi1996@icloud.com>
2018-10-18 14:37:57 +0800

    添加了Swagger2网页restful文档的支支支持——见TestController

--- src/main/java/com/masaiqi/controller/TestController.java
+++ src/main/java/com/masaiqi/controller/TestController.java
@@ -2,17 +2,28 @@ package com.masaiqi.controller;
 
 import com.masaiqi.dao.UserMapper;
 import com.masaiqi.entity.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/testboot")
+@Api("测试2类")
 public class TestController {
     @Autowired
     UserMapper userMapper;
 
-    @RequestMapping("getUser")
+    @ApiOperation(value="更新信息", notes="根据url的id来指定更新用户信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long",paramType = "path"),
+            @ApiImplicitParam(name = "user", value = "用户实体user", required = true, dataType = "User")
+    })
+    @RequestMapping(value = "getUser",method = RequestMethod.POST)
     public User getUser() {
         User user = userMapper.selectByPrimaryKey(1);
         return user;
